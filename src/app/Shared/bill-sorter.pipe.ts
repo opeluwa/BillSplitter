1b19f9fc338f1417cb80b27a4d3528d868aaae6f
ope <opeoluwaalase@gmail.com>
2019-09-08 15:18:29 +0100

    update to more efficent code
    efficent code produced

--- src/app/Shared/bill-sorter.pipe.ts
+++ src/app/Shared/bill-sorter.pipe.ts
@@ -14,49 +14,41 @@ export class BillSorterPipe implements PipeTransform {
     const instanceCheck = this.checkInstance(array);
     if (instanceCheck === 0) {  // type is of UserBillModel[]
       return array.sort((n1, n2) => {
+        const n1DateDue = Math.abs(n1.mainBill.dateDue);
+        const n2DateDue = Math.abs(n2.mainBill.dateDue);
+        const n1billName = n1.mainBill.billName.toLowerCase();
+        const n2billName = n2.mainBill.billName.toLowerCase();
+        const n1SharedCost = n1.mainBill.cost / n1.mainBill.numOfPayers;
+        const n2SharedCost = +n2.mainBill.cost / +n1.mainBill.numOfPayers;
         if (search === 1) {
-          const n1DateDue = Math.abs(n1.mainBill.dateDue);
-          const n2DateDue = Math.abs(n2.mainBill.dateDue);
           return n1DateDue < n2DateDue ? -1 : 1;
         } else if (search === 2) {
-          const n1DateDue = Math.abs(n1.mainBill.dateDue);
-          const n2DateDue = Math.abs(n2.mainBill.dateDue);
           return n1DateDue > n2DateDue ? -1 : 1;
         } else if (search === 3) {
-          const n1billName = n1.mainBill.billName;
-          const n2billName = n2.mainBill.billName;
           return n1billName < n2billName ? -1 : 1;
         } else if (search === 4) {
-          const n1SharedCost = n1.mainBill.cost / n1.mainBill.numOfPayers;
-          const n2SharedCost = +n2.mainBill.cost / +n1.mainBill.numOfPayers;
           return n1SharedCost < n2SharedCost ? -1 : 1;
         } else if (search === 5) {
-          const n1SharedCost = n1.mainBill.cost / n1.mainBill.numOfPayers;
-          const n2SharedCost = +n2.mainBill.cost / +n1.mainBill.numOfPayers;
           return n1SharedCost > n2SharedCost ? -1 : 1;
         }
       });
     } else if (instanceCheck === 1) {  // type is of PaidModel[]
         return array.sort((n1, n2) => {
+          const n1DateDue = Math.abs(n1.dateDue);
+          const n2DateDue = Math.abs(n2.dateDue);
+          const n1billName = n1.billName.toLowerCase();
+          const n2billName = n2.billName.toLowerCase();
+          const n1SharedCost = n1.cost / n1.numOfPayers;
+          const n2SharedCost = n2.cost / n2.numOfPayers;
           if (search === 1) {
-            const n1DateDue = Math.abs(n1.dateDue);
-            const n2DateDue = Math.abs(n2.dateDue);
             return n1DateDue < n2DateDue ? -1 : 1;
           } else if (search === 2) {
-            const n1DateDue = Math.abs(n1.dateDue);
-            const n2DateDue = Math.abs(n2.dateDue);
             return n1DateDue > n2DateDue ? -1 : 1;
           } else if (search === 3) {
-            const n1billName = n1.billName;
-            const n2billName = n2.billName;
             return n1billName < n2billName ? -1 : 1;
           } else if (search === 4) {
-            const n1SharedCost = n1.cost / n1.numOfPayers;
-            const n2SharedCost = n2.cost / n1.numOfPayers;
             return n1SharedCost < n2SharedCost ? -1 : 1;
           } else if (search === 5) {
-            const n1SharedCost = n1.cost / n1.numOfPayers;
-            const n2SharedCost = n2.cost / n1.numOfPayers;
             return n1SharedCost > n2SharedCost ? -1 : 1;
           }
         });
